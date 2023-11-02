ea6064daf7134cdffbb056ada1e152e9ff019782
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-02 21:20:55 +0100

    1way databinding component Ts- html interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppointmentAppComponent } from './appointment-app/appointment-app.component';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppointmentAppComponent
+    AppointmentListComponent,
+    
   ],
   imports: [
     BrowserModule
