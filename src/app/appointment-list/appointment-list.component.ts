ad40b8af271af22e339c6fad36a43829569fd716
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-03 09:46:40 +0100

    added delete appointment button functionality

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -26,7 +26,11 @@ export class AppointmentListComponent {
       this.newAppointmentTitle = "";
       this.newAppointmentDate = new Date()
 
-      alert(this.appointments.length)
+      
     }
+  }
+
+  deleteAppointment(index:number){
+    this.appointments.splice(index,1)
   }
 }
